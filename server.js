5ce5e066bafac4ac5560ce49e44e141c25ff2e76
uv66 <53874064+uv66@users.noreply.github.com>
2020-06-30 13:10:50 +0530

    Add files via upload

---
+++ server.js
@@ -0,0 +1,7 @@
+const express = require('express');
+const port = 3000;
+const app = express();
+app.listen(port, function (){console.log("Server is running on "+ port +" port");});
+app.get('/', function (req, res) {
+  res.send('<h1>Hello World! sruthiiiiiiiiii</h1>')
+})   
