eb0ddc8913568595ce960e44ec4633b8c1539bf5
uv66 <53874064+uv66@users.noreply.github.com>
2020-06-30 13:46:56 +0530

    Update server.js

--- server.js
+++ server.js
@@ -3,5 +3,5 @@ const port = 3000;
 const app = express();
 app.listen(port, function (){console.log("Server is running on "+ port +" port");});
 app.get('/', function (req, res) {
-  res.send('<h1>Hello World! sruthiiiiiiiiii</h1>')
+  res.send('<h1>Hello World!</h1>')
 })   
